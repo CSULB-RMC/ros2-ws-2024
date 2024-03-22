fb0b0f5d558bb599a85ebbeaf71f2e51414e7648
William Ho <howilliam129@gmail.com>
2024-03-21 17:39:02 -0700

    create camera test branch

--- build/camera_test/prefix_override/sitecustomize.py
+++ build/camera_test/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/beachlbl/ros2-ws-2024/install/camera_test'
+sys.prefix = sys.exec_prefix = '/root/ros2-ws-2024/install/camera_test'
