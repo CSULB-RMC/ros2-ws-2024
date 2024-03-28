3fca7249f5584856675b1a2465265be5fe6d1336
Leo Pan <leo.long.pan@gmail.com>
2024-03-28 03:31:33 +0000

    comments

--- src/drive_ex/drive_ex/drivetrain_ex_sub.py
+++ src/drive_ex/drive_ex/drivetrain_ex_sub.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from std_msgs.msg import UInt8
+#ignore can import error if it's there, it works if you installed python-can
 import can
 
 
@@ -9,28 +10,43 @@ class DrivetrainExcavator(Node):
     
     def __init__(self):
         super().__init__('drivetrain_excavator')
+
+        #publishes can messages of its current state periodically
+        #currently every 0.02 seconds, so 50 times a second
         self.canSend = self.create_timer(0.02, self.can_callback)
 
+        #create subscribers to listen for teleop computer commands
         self.ex_dt_left_sub = self.create_subscription(UInt8, 'ex_dt_left', self.ex_dt_left_update, 10)
         self.ex_dt_right_sub = self.create_subscription(UInt8, 'ex_dt_right', self.ex_dt_right_update, 10)
 
+        #create state variables, these keep track of what motors
+        #should be running and how fast at the current moment
         self.ex_dt_left_speed = 0
         self.ex_dt_right_speed = 0
 
+        #create can bus link, right now is linked to virtual vcan 0, most likely
+        #will be can0 when on the bot
         self.bus = can.interface.Bus(interface='socketcan', channel='vcan0', bitrate='50000')
 
         self.i = 0
 
+    #updates the states of the left drivetrain motors
     def ex_dt_left_update(self, msg):
+        #msg is an UInt8 from 0-200
         #TODO
+        self.ex_dt_left_speed = 0
         self.get_logger().info('updating left ex dt')
         pass
 
+    #updates the states of the right drivetrains motors
     def ex_dt_right_update(self, msg):
+        #msg is an UInt8 from 0-200
         #TODO
+        self.ex_dt_right_speed = 0
         self.get_logger().info('updating right ex dt')
         pass
 
+    #periodically pushes out can messages to keep the rover running
     def can_callback(self):
         #TODO
         msg = can.Message(
