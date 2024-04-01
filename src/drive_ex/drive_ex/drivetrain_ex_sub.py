331422788a008c9b6957a8bbf5318c307f8d63ff
porterclev <porterclev@gmail.com>
2024-04-01 14:48:52 -0700

    can messages

--- src/drive_ex/drive_ex/drivetrain_ex_sub.py
+++ src/drive_ex/drive_ex/drivetrain_ex_sub.py
@@ -33,50 +33,84 @@ class DrivetrainExcavator(Node):
         self.bus = can.interface.Bus(interface='socketcan', channel='vcan0', bitrate='50000')
 
         self.i = 0
-
+    
     #updates the states of the left drivetrain motors
     def ex_dt_left_update(self, msg):
         #msg is an UInt8 from 0-200
         #TODO
-        self.ex_dt_left_speed = 0
-        self.get_logger().info('updating left ex dt')
-        pass
+        # self.get_logger().info('updating left ex dt')
+        self.ex_dt_left_speed = msg.data.to_bytes(8, 'big')
+
+        can_msg = can.Message(
+                arbitration_id=0xd0,
+                data = self.ex_dt_left_speed,
+                )
+        
+        self.bus.send(can_msg)
+        self.get_logger().info(f'{can_msg}')
+        # self.get_logger().info(f'{msg.data}, {controller_data}')
 
     #updates the states of the right drivetrains motors
     def ex_dt_right_update(self, msg):
         #msg is an UInt8 from 0-200
         #TODO
-        self.ex_dt_right_speed = 0
-        self.get_logger().info('updating right ex dt')
-        pass
+
+        self.ex_dt_right_speed =  msg.data.to_bytes(8, 'big')
+        # self.get_logger().info('updating right ex dt')
+       
+        can_msg = can.Message(
+                arbitration_id=0xd1,
+                data = self.ex_dt_right_speed 
+                )
+
+        self.bus.send(can_msg)
+        self.get_logger().info(f'{can_msg}')
 
     def ex_excavator_update(self, msg):
         #msg is an UInt8 from 0-200
         #TODO
-        self.ex_excavator_speed = 0
-        self.get_logger().info('updating excavator')
-        pass
+        self.ex_excavator_speed = msg.data.to_bytes(8, 'big')
+        # self.get_logger().info('updating excavator')
+        
+        can_msg = can.Message(
+                arbitration_id=0xe0,
+                data = self.ex_excavator_speed 
+                )
+
+        self.bus.send(can_msg)
+        self.get_logger().info(f'{can_msg}')
+
 
     def ex_reg_update(self, msg):
         #msg is an UInt8 from 0-200
         #TODO
-        self.ex_reg_speed = 0
-        self.get_logger().info('updating regolith')
-        pass
+        self.ex_reg_speed = msg.data.to_bytes(8, 'big')
+        # self.get_logger().info('updating regolith')
+
+        can_msg = can.Message(
+                arbitration_id=0xb0,
+                data = self.ex_reg_speed 
+                )
+
+
+        self.bus.send(can_msg) 
+        self.get_logger().info(f'{can_msg}')
 
     #periodically pushes out can messages to keep the rover running
     def can_callback(self):
         #TODO
         msg = can.Message(
             arbitration_id=0xC0FFEE,
-            data=[0, 25, 0, 1, 3, 1, 4, self.i],
+            data=[0, 25, 0, 1, 3, 1, 4],
             is_extended_id=True
         )
         self.bus.send(msg)
+        # self.get_logger().info(f'{msg.data}')
         self.i += 1
         pass
 
 def main(args=None):
+    print("Bus Publisher Active")
     rclpy.init(args=args)
     node = DrivetrainExcavator()
     rclpy.spin(node)
