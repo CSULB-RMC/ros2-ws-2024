d4e62c54a071e3672857eefced7c5414f042bd6c
porterclev <porterclev@gmail.com>
2024-04-01 17:14:16 -0700

    added stop signal

--- src/joy_control/joy_control/joy_pub.py
+++ src/joy_control/joy_control/joy_pub.py
@@ -11,7 +11,7 @@ class JoyPub(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.dt_left_publisher_ = self.create_publisher(UInt8, 'ex_dt_left', 10)
+        self.dt_l_publisher_ = self.create_publisher(UInt8, 'ex_dt_left', 10)
         self.dt_r_publisher_ = self.create_publisher(UInt8, 'ex_dt_right', 10)
         self.ex_publisher_ = self.create_publisher(UInt8, 'ex_excavator', 10)
         self.reg_publisher_ = self.create_publisher(UInt8, 'ex_reg', 10)
@@ -25,29 +25,37 @@ class JoyPub(Node):
         uint8 = UInt8()
         
         # Left Stick Maps - Left Drive Train
-        if msg.axes[1] > 0: # L Stick Up
-            uint8.data = 150
-            self.dt_left_publisher_.publish(uint8)
-            self.get_logger().info("L-stick: Up")
+        if msg.axes[1] > 0.01: # L Stick Up 
+            uint8.data = int((msg.axes[1] * 100) + 100) # add 100 to indicate forward motion and not include 100
+            self.dt_l_publisher_.publish(uint8)
+            self.get_logger().info(f'L-stick: Up, {uint8.data}')
 
         elif msg.axes[1] < 0: # L Stick Down 
-            uint8.data = 50
-            self.dt_left_publisher_.publish(uint8)
-            self.get_logger().info("L-stick: Down")
+            uint8.data = int((abs(msg.axes[1]) * 100) - 1) # subtract 1 to no include 100 
+            self.dt_l_publisher_.publish(uint8)
+            self.get_logger().info(f'L-stick: Down, {uint8.data}')
         
-        # Right Stick Maps - Right Drive Train
-        if msg.axes[3] > 0: # R Stick Up
-            uint8.data = 150
-            self.dt_r_publisher_.publish(uint8)
-            self.get_logger().info("R-stick: Up")
+        else: # Left Drive Train Not Moving
+            uint8.data = 100 # stop value 
+            self.dt_l_publisher_.publish(uint8)
 
-        elif msg.axes[3] < 0: # R Stick Down
-            uint8.data = 50
+        # Right Stick Maps - Right Drive Train
+        if msg.axes[4] > 0.01: # R Stick Up
+            uint8.data = int((msg.axes[4] * 100) + 100) # add 100 to indicate forward motion and not include 100
             self.dt_r_publisher_.publish(uint8)
-            self.get_logger().info("R-stick: Down")
+            self.get_logger().info(f'R-stick: Up, {uint8.data}')
+
+        elif msg.axes[4] < 0: # R Stick Down
+            uint8.data = int((abs(msg.axes[4]) * 100) - 1) # subtract 1 to no include 100 
+            self.dt_r_publisher_.publish(uint8)
+            self.get_logger().info(f'R-stick: Down, {uint8.data}')
         
+        else: # Right Drive Train Not Moving
+            uint8.data = 100 # stop value 
+            self.dt_r_publisher_.publish(uint8)
+
         # D pad Maps - Excavator
-        if msg.axes[7] > 0: # D pad Up
+        if msg.axes[7] > 0.01: # D pad Up
             uint8.data = 150
             self.ex_publisher_.publish(uint8)
             self.get_logger().info("Dpad: up")
@@ -57,13 +65,18 @@ class JoyPub(Node):
             self.ex_publisher_.publish(uint8)
             self.get_logger().info("Dpad: down")
         
+        else:
+            uint8.data = 100
+            self.ex_publisher_.publish(uint8)
+
         # A button
+        # TODO place holder 
         if msg.buttons[0] == 1:
             uint8.data = 150
             self.reg_publisher_.publish(uint8)
             self.get_logger().info("A: pressed")
 
-        elif msg.buttons[0] == 1:
+        elif msg.buttons[0] == 0:
             uint8.data = 50
             self.reg_publisher_.publish(uint8)
 
@@ -71,6 +84,7 @@ class JoyPub(Node):
        pass 
 
 def main(args=None):
+    print("Controller Active")
     rclpy.init(args=args)
 
     joy_node = JoyPub()
