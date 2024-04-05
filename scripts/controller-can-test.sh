13d1d00558f2f90753fc81b0bf1f571d094a453f
porterclev <porterclev@gmail.com>
2024-04-05 16:17:48 -0700

    made scripts file

---
+++ scripts/controller-can-test.sh
@@ -0,0 +1,11 @@
+echo "Starting Joy Listener"
+ros2 run joy joy_node --ros-args -p autorepeat_rate:=0.0 &
+sleep 3
+echo "Starting Controller Interface"
+colcon build --packages-select drive_ex && ros2 run drive_ex drivetrain_ex &
+sleep 7
+echo "Starting drivetrain_ex"
+colcon build --packages-select joy_control && ros2 run joy_control talker &
+sleep 7
+echo "Starting Can View"
+candump vcan0
