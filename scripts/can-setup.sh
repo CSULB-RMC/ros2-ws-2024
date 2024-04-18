6313bc21afd7df22c790c1129c84bea1d7dfa8e5
porterclev <porterclev@gmail.com>
2024-04-18 15:00:48 -0700

    specified bash in scripts

--- scripts/can-setup.sh
+++ scripts/can-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo ip link set up can0 type can bitrate 500000
 sudo ip link set can0 txqueuelen 1000
 sudo ip link set up can0
--- scripts/vcan-setup.sh
+++ scripts/vcan-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan bitrate 500000
 sudo ip link set vcan0 txqueuelen 1000
