7bc3645f7a248a06b9f93128c4400c152bb70003
porterclev <porterclev@gmail.com>
2024-04-22 12:16:05 -0700

    ui

--- src/ui/ui/ui_health.py
+++ src/ui/ui/ui_health.py
@@ -14,8 +14,17 @@ class UI(Node):
         self.status_timer_fail = self.create_timer(5, self.timer_fail_callback)
         self.cb_sub = self.create_subscription(String, 'cb_active', self.cb_ui_update, 10)
         self.cb_sub = self.create_subscription(String, 'ex_active', self.ex_ui_update, 10)
+        
+        # self.ex_dt_left_sub = self.create_subscription(UInt8, 'ex_dt_left', self.ex_dt_left_update, 10)
+        # self.ex_dt_right_sub = self.create_subscription(UInt8, 'ex_dt_right', self.ex_dt_right_update, 10)
+        self.ex_conveyer_sub = self.create_subscription(UInt8, 'ex_conveyer', self.ex_conveyer_update, 10)
+        # self.ex_arm_sub = self.create_subscription(UInt8, 'ex_arm', self.ex_arm_update, 10)
+        # self.ex_digger_sub = self.create_subscription(UInt8, 'ex_digger', self.ex_digger_update, 10)
+
+
         self.cb_health = "Dead"
         self.ex_health = "Dead"
+        self.health_msg = ""
 
     def cb_ui_update(self, msg):
         self.cb_health = msg.data
@@ -23,9 +32,12 @@ class UI(Node):
     def ex_ui_update(self, msg):
         self.ex_health = msg.data
 
+    def ex_conveyer_update(self, msg):
+        self.health_msg = f'{msg.data}'
+
     def timer_callback(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'Excavator Bot: {self.ex_health}')
+        print(f'Excavator Bot: {self.ex_health}, conveyer speed: {self.health_msg}')
         print(f'Cargo Bot: {self.cb_health}')
     
     def timer_fail_callback(self):
