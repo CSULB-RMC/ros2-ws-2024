6ed2e7e92ba88c1726177c9bd836708fc095a1b7
porterclev <porterclev@gmail.com>
2024-04-24 20:32:33 -0700

    dual camera action

--- src/camera_test/camera_test/cam_pub.py
+++ src/camera_test/camera_test/cam_pub.py
@@ -18,7 +18,7 @@ class ImagePublisher(Node):
 			self.listener_callback,
 			10)
         self.cap = cv2.VideoCapture(0)
-        # self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('P', 'N', 'G', ' '))
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.cv_bridge = CvBridge()
         self.i = 0
@@ -28,9 +28,9 @@ class ImagePublisher(Node):
         self.FPS = 1/30
         self.FPS_MS = int(self.FPS * 1000)
 
-        # self.thread = Thread(target = self.update, args = ())
-        # self.thread.daemon = True
-        # self.thread.start()
+        self.thread = Thread(target = self.update, args = ())
+        self.thread.daemon = True
+        self.thread.start()
 
         self.status = False
         self.frame  = None
@@ -55,7 +55,7 @@ class ImagePublisher(Node):
             self.timer_callback()
 
 def main(args=None):
-    print("Camera On")
+    print("Camera On1")
     rclpy.init(args=args)
     node = ImagePublisher()
     rclpy.spin(node)
